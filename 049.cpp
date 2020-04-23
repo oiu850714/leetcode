2306d874b136be367a4639a9cf564495196f7444
oiu850714 <oiu850714@gmail.com>
2020-04-23 21:33:30 +0800

    add solution for problem 049

---
+++ 049.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+#include <map>
+
+using AnagramGroups = std::map<std::vector<int>, std::vector<std::string>>;
+
+class Solution {
+public:
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& Strs) {
+        // lowercase -> 26 ascii
+        // 如果 utf8，那應該還是乖乖 sort
+        AnagramGroups Groups;
+        for (const auto &Str : Strs) {
+            Groups[countedCategory_(Str)].push_back((Str));
+        }
+
+        std::vector<std::vector<std::string>> Ret;
+        for (const auto &Group : Groups) {
+            Ret.push_back(Group.second);
+        }
+        return Ret;
+    }
+
+    std::vector<int> countedCategory_(const std::string &Str) {
+        std::vector<int> Count(26);
+        for (auto c : Str) {
+            ++Count[c - 'a'];
+        }
+
+        return Count;
+    }
+};
